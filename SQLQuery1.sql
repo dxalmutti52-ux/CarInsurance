e96971e2a5a9cb7dc7a8b7fa4bc058c184614cbe
dxalmutti52-ux <dxalmutti52@gmail.com>
2026-05-05 22:29:54 -0400

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Insurees] (
+    [Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [FirstName] NVARCHAR(50) NOT NULL,
+    [LastName] NVARCHAR(50) NOT NULL,
+    [EmailAddress] NVARCHAR(100) NOT NULL,
+    [DateOfBirth] DATETIME NOT NULL,
+    [CarYear] INT NOT NULL,
+    [CarMake] NVARCHAR(50) NOT NULL,
+    [CarModel] NVARCHAR(50) NOT NULL,
+    [DUI] BIT NOT NULL,
+    [SpeedingTickets] INT NOT NULL,
+    [CoverageType] BIT NOT NULL,
+    [Quote] DECIMAL(18,2) NOT NULL
+);
